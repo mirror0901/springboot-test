febf8f5dcd02646975680dfdceaca9959056b918
mirror0901 <1755496180@qq.com>
2018-09-02 16:38:10 +0800

    项目初始化

---
+++ src/main/java/top/mirror95/springboottest/SpringbootTestApplication.java
@@ -0,0 +1,12 @@
+package top.mirror95.springboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTestApplication.class, args);
+	}
+}
